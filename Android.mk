7bc079d8e267cb4960c5efde6d5f5fd12a6054ce
Carlosine <1874990240@qq.com>
2025-11-28 10:35:23 +0800

    update

--- Android.mk
+++ Android.mk
@@ -8,6 +8,12 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),OP4A47)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
-endif
 
+# 覆盖 dtb/dtbo 的安装目标，避免 ninja 去找 out/.../dtb.img
+INSTALLED_DTBIMAGE_TARGET := $(BOARD_PREBUILT_DTBIMAGE)
+INSTALLED_DTBOIMAGE_TARGET := $(BOARD_PREBUILT_DTBOIMAGE)
+
+# 递归包含子目录的 makefile
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+endif
