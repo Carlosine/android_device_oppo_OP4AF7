6c767dfcff5d62cd9b3ccf0b1ba9bc3b3eb02c0f
Carlosine <1874990240@qq.com>
2025-11-28 09:43:06 +0800

    update

--- Android.mk
+++ Android.mk
@@ -8,13 +8,6 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),OP4A47)
-
-# 覆盖 dtb.img 的生成规则，直接使用预编译文件
-INSTALLED_DTBIMAGE_TARGET := $(BOARD_PREBUILT_DTBIMAGE)
-
-# 如果有预编译 dtbo，也覆盖
-INSTALLED_DTBOIMAGE_TARGET := $(BOARD_PREBUILT_DTBOIMAGE)
-
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
 
